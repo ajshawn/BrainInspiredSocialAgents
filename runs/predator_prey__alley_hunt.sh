d32a690520bee2e33919b2633b00c97e6a36e927
ajshawn <dyx723@g.ucla.edu>
2024-12-15 17:03:56 -0800

    Add training starting script for other predator&prey substrates

--- runs/predator_prey__alley_hunt.sh
+++ runs/predator_prey__alley_hunt.sh
@@ -1,5 +1,4 @@
 GPUS="4,5,6,7"
-EXP_DIR="/local/shawn/marl-jax/results/PopArtIMPALA_1_meltingpot_predator_prey__open_2024-11-26_17:36:18.023323"
 
 CUDA_VISIBLE_DEVICES=${GPUS} python train.py \
   --async_distributed \
@@ -7,6 +6,6 @@ CUDA_VISIBLE_DEVICES=${GPUS} python train.py \
   --num_actors 26 \
   --algo_name PopArtIMPALA \
   --env_name meltingpot \
-  --map_name predator_prey__open \
+  --map_name predator_prey__alley_hunt \
   --seed 1 \
   --experiment_dir ${EXP_DIR}
