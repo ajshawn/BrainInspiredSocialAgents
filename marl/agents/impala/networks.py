78dffc4db2999fbbe7ad78e56833f410ec27558f
kumquat <linfangu@ucla.edu>
2026-01-25 21:33:01 -0800

    Revert "add gated mechanism"
    This reverts commit 4598ecb661a10f8b7e9c29a605c018a08132c59f.

--- marl/agents/impala/networks.py
+++ marl/agents/impala/networks.py
@@ -684,10 +684,6 @@ class MultiHeadAttentionLayer(hk.Module):
         hk.Linear(64),
         jax.nn.relu,
     ])
-    self.gate_ff = hk.Sequential([
-      hk.Linear(self.model_dim),
-      jax.nn.sigmoid,
-    ])
 
   def __call__(self, query, key, value, enhance_map=jnp.zeros((1, 121))):
     if jnp.ndim(query) == 1:
@@ -796,9 +792,7 @@ class MultiHeadAttentionLayer(hk.Module):
     output = output.reshape(output.shape[0], -1)  # [B, model_dim]
 
     # Additional nonlinear transformations after attention
-    #output = self._ff(output)
-    gate = self.gate_ff(query)
-    output = gate * output
+    output = self._ff(output)
 
     return output, weights
 
